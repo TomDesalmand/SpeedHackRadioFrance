b294f0288c6fe03581cadaf7914c527cfa0288f6
Tom <tom.desalmand@epitech.eu>
2023-10-18 15:26:20 +0200

    [CHANGE] Optimized functions & changed the way it worked to take any keywords

--- api.py
+++ api.py
@@ -1,27 +1,30 @@
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def getOccurenceFranceCulture():
-    dataFranceCulture = pd.read_csv("./data/grid/franceculture.csv")
-    filteredDataFranceCulture = dataFranceCulture[dataFranceCulture["diffusion_standfirst"].str.contains("développement durable|réchauffement climatique|crise énergétique", case=False, na=False, regex=True)]
-    return len(filteredDataFranceCulture["diffusion_standfirst"])
-
-def getOccurenceFranceInfo():
-    dataFranceInfo = pd.read_csv("./data/grid/franceinfo.csv")
-    filteredDataFranceInfo = dataFranceInfo[dataFranceInfo["diffusion_standfirst"].str.contains("développement durable|réchauffement climatique|crise énergétique", case=False, na=False, regex=True)]
-    return len(filteredDataFranceInfo["diffusion_standfirst"])
-
-def getOccurenceFranceInter():
-    dataFranceInter = pd.read_csv("./data/grid/franceinter.csv")
-    filteredDataFranceInter = dataFranceInter[dataFranceInter["diffusion_standfirst"].str.contains("développement durable|réchauffement climatique|crise énergétique", case=False, na=False, regex=True)]
-    return len(filteredDataFranceInter["diffusion_standfirst"])
+def getOccurrence(data_file, keywords):
+    data = pd.read_csv(data_file)
+    filtered_data = data[data["diffusion_standfirst"].str.contains('|'.join(keywords), case=False, na=False, regex=True)]
+    return len(filtered_data["diffusion_standfirst"])
 
 def main():
-    labels = ['France Culture', 'France Info', 'France Inter']
-    sizes = [getOccurenceFranceCulture(), getOccurenceFranceInfo(), getOccurenceFranceInter()]
+    parser = argparse.ArgumentParser(description="Calculate and plot climate-related program occurrences in different radio stations.")
+    parser.add_argument("--keywords", nargs='+', help="List of keywords to search for in program descriptions", required=True)
+    args = parser.parse_args()
+    
+    radio_data = [
+        {"name": "France Culture", "file": "./data/grid/franceculture.csv"},
+        {"name": "France Info", "file": "./data/grid/franceinfo.csv"},
+        {"name": "France Inter", "file": "./data/grid/franceinter.csv"}
+    ]
+    keywords = args.keywords
+
+    labels = [radio["name"] for radio in radio_data]
+    sizes = [getOccurrence(radio["file"], keywords) for radio in radio_data]
+
     plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140)
     plt.axis('equal')
-    plt.title("Occurrences of climate-related programms in different radios")
+    plt.title("Occurrences of climate-related programs in different radios")
     plt.savefig('climate_occurrences.png')
     plt.close()
 
